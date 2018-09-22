d6132f664166026719317f2d8906ea48f5afc073
Li Hao <ash.lihao@gmail.com>
2018-09-22 17:12:58 +0800

    minor change of tests

--- example/mds/main.go
+++ example/mds/main.go
@@ -1,4 +1,4 @@
-package mds
+package main
 
 import (
 	. "bean"
@@ -11,12 +11,24 @@ func main() {
 	mds := bean.NewRPCMDSConnC("tcp", "13.229.125.250:9892")
 	pair := Pair{BTC, USDT}
 	for {
-		start := time.Now()
-		time.Sleep(time.Second * 10)
 		end := time.Now()
-		ob, _ := mds.GetOrderBookTS(pair, start, end, 2)
+		start := end.Add(time.Duration(-10)*time.Minute)
+		fmt.Println("Orderbook history from", start, "to", end)
+		obts, _ := mds.GetOrderBookTS(pair, start, end, 20)
+		fmt.Println(time.Now().Sub(end))
+
+		printOrderBookTS(obts)
+
+		fmt.Println("Transaction history from", start, "to", end)
 		txn, _ := mds.GetTransactions(pair, start, end)
-		fmt.Println(ob)
+
 		fmt.Println(txn)
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func printOrderBookTS (obts OrderBookTS) {
+	for _, ob := range obts {
+		fmt.Println(ob.Time, len(ob.OB.Asks), "bestBid:", ob.OB.Bids[0].Price, "bestAsk:", ob.OB.Asks[0].Price)
 	}
 }
