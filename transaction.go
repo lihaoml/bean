34dbeafacffee42d85c25e7fe6ccd2ab4186cf15
Li Hao <ash.lihao@gmail.com>
2018-10-15 00:25:32 +0800

    sugar functions for Transactions

--- transaction.go
+++ transaction.go
@@ -40,8 +40,8 @@ func (t Transactions) Sort() Transactions {
 }
 
 // get transactions up to t, assuming txn is sorted
-func (txn Transactions) GetTransactionHistory(t time.Time) Transactions {
-	var idx int
+func (txn Transactions) Upto(t time.Time) Transactions {
+	idx := 0
 	for i, tt := range txn {
 		if tt.TimeStamp.Before(t) {
 			idx = i
@@ -49,6 +49,50 @@ func (txn Transactions) GetTransactionHistory(t time.Time) Transactions {
 			break
 		}
 	}
-	res := txn[0:idx]
+	res := txn[0:idx+1]
 	return res
 }
+
+// get transactions in a time interval, assuming txn is sorted
+func (txn Transactions) Between(from, to time.Time) Transactions {
+	var res Transactions
+	startIdx := len(txn)
+	for i, tt := range txn {
+		if !tt.TimeStamp.Before(from) {
+			startIdx = i
+			break
+		}
+	}
+	if startIdx < len(txn) {
+		endIdx := startIdx
+		for i := startIdx; i < len(txn); i++ {
+			if txn[i].TimeStamp.Before(to) {
+				endIdx = i
+			} else {
+				break
+			}
+		}
+		res = txn[startIdx:endIdx+1]
+	}
+	return res
+}
+
+func (txn Transactions) Cross (price, amount float64) bool {
+	if amount < 0 {
+		// selling, so need to check if the highest transaction is larger than the order price
+		for _, t := range txn {
+			if t.Price > price {
+				return true
+			}
+		}
+	} else {
+		// buying, so need to check if the lowest transaction is lower than the order price
+		for _, t := range txn {
+			if t.Price < price {
+				return true
+			}
+		}
+	}
+	return false
+}
+
