7089eef88b8812f64fbe75d687fa4276445d6d20
fujiegg <gzqgg111@gmail.com>
2020-01-16 13:19:19 +0800

    add vendor

--- vendor/github.com/influxdata/influxdb/flux/stdlib/packages.go
+++
@@ -1,7 +0,0 @@
-package stdlib
-
-// Import all stdlib packages
-import (
-	_ "github.com/influxdata/influxdb/flux/stdlib/influxdata/influxdb"
-	_ "github.com/influxdata/influxdb/flux/stdlib/influxdata/influxdb/v1"
-)
